167db02b935e100d6adaf4f3adeeb4ec871d0e6a
Tanya Bhagat <96003089+t-a-n-ya@users.noreply.github.com>
2023-03-10 11:53:18 +0530

    Update Database_connection.js

--- database/Database_connection.js
+++ database/Database_connection.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_DBNAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    define: {
-      freezeTableName: true
-    },
-  },
-);
+const sequelize = new Sequelize('postgres://hospital_zmko_user:9XJzJMgYgTwCQSfVQfFjl6Iuyn1jnkpq@dpg-cg4asl2k728m6o5krhu0-a.oregon-postgres.render.com/hospital_zmko');
+//   process.env.DB_DBNAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     define: {
+//       freezeTableName: true
+//     },
+//   },
+// );
 
 sequelize
   .authenticate()
