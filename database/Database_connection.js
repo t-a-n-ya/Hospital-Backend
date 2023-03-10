d665bc19c84aed6d834b2ed29aa0799f0a34581a
Tanya Bhagat <96003089+t-a-n-ya@users.noreply.github.com>
2023-03-10 17:24:16 +0530

    Update Database_connection.js

--- database/Database_connection.js
+++ database/Database_connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://hospital_zmko_user:9XJzJMgYgTwCQSfVQfFjl6Iuyn1jnkpq@dpg-cg4asl2k728m6o5krhu0-a.oregon-postgres.render.com/hospital_zmko');
+// const sequelize = new Sequelize('postgres://hospital_zmko_user:9XJzJMgYgTwCQSfVQfFjl6Iuyn1jnkpq@dpg-cg4asl2k728m6o5krhu0-a.oregon-postgres.render.com/hospital_zmko');
 //   process.env.DB_DBNAME,
 //   process.env.DB_USER,
 //   process.env.DB_PASSWORD,
@@ -13,6 +13,20 @@ const sequelize = new Sequelize('postgres://hospital_zmko_user:9XJzJMgYgTwCQSfVQ
 //     },
 //   },
 // );
+const sequelize = new Sequelize({
+  database: "hospital_zmko",
+  username: "hospital_zmko_user",
+  password: "9XJzJMgYgTwCQSfVQfFjl6Iuyn1jnkpq",
+  host: "dpg-cg4asl2k728m6o5krhu0-a",
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  },
+});
 
 sequelize.authenticate().then(() => {
   console.log("Success!");
